f4fd5f8b79e8b4d775e3778ffce9319e886e860a
Muralidhar Chavan <muralidhar.chavan@gmail.com>
2020-11-24 16:17:46 +0530

    change number of news items

--- BackendService-NewsAPI/samples.js
+++ BackendService-NewsAPI/samples.js
@@ -3,7 +3,7 @@ generic_query.query = "(enriched_text.categories.label:'finance'|enriched_text.c
 //generic_query.filter = "language:(english|en),publication_date>now-1week,(enriched_text.concepts.text:'equity'|enriched_text.concepts.text:'stock'), host:wsj.com | host: cmlviz.com";
 generic_query.filter = "language:(english|en),(enriched_text.concepts.text:'equity'|enriched_text.concepts.text:'stock')";
 //generic_query.deduplicate = true;
-generic_query.count = 5;
+generic_query.count = 10;
 generic_query.return = "title,url,host,publication_date"
 
 
@@ -11,7 +11,7 @@ var sample_attributes = {
     language: "en",
     hosts: ["wsj.com", "cmlviz.com"],
     interested_in: ["equity", "stock"],
-    count: 5,
+    count: 10,
     return: "title,url,host,publication_date",
     //deduplicate: true
 };
